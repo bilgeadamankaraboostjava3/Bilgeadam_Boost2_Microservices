2c75f0e05930c148d79d3588bc56f550c8693240
bilgeadamankaraboostjava3 <107560948+bilgeadamankaraboostjava3@users.noreply.github.com>
2022-09-27 14:23:55 +0300

    Exception Handler Eklendi. Bu şekilde kullanalım

--- auth-service/src/main/java/com/muhammet/controller/AuthController.java
+++ auth-service/src/main/java/com/muhammet/controller/AuthController.java
@@ -1,11 +1,16 @@
 package com.muhammet.controller;
 
+import com.muhammet.dto.request.DoLoginRequestDto;
+import com.muhammet.dto.request.RegisterRequestDto;
 import com.muhammet.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 import static com.muhammet.constants.ApiUrls.*;
 
@@ -16,11 +21,11 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping(LOGIN)
-    public ResponseEntity<Void> doLogin(){
+    public ResponseEntity<Void> doLogin(DoLoginRequestDto dto){
         return null;
     }
-    @PostMapping(REGISTER)
-    public ResponseEntity<Void> register(){
+    @GetMapping(REGISTER)
+    public ResponseEntity<Void> register(@Valid RegisterRequestDto dto){
         return null;
     }
 
